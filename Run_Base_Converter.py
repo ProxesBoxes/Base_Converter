e8437a68658906a8ad6bd7ca7f9432e319568a54
Prox <prox.darknet@gmail.com>
2020-08-13 12:45:00 -0400

    first go at some simplistic regex matching for dtecting charset provided.

--- Run_Base_Converter.py
+++ Run_Base_Converter.py
@@ -66,22 +66,26 @@ def print_and_get_character_set():
 def populate_from_console(lets_convert):
 
     parser = argparse.ArgumentParser(prog="Run_Base_Converter",
-                                     description="A base converter tool which supports different character sets. The "
-                                                 "different character sets supported currently are: " +
+                                     description="A base converter tool which supports different character sets. "
+                                                 "Currently only positive values, and positive bases are supported. The"
+                                                 " different character sets supported currently are: " +
                                                  Charsets.predefined_charsets.__str__()[1:-1] + ".",
                                      epilog="Built for The Information Technology Syndicate")
 
     parser.add_argument("-sb", "--starting_base", help="The base in which the value to convert from, default is base 10"
                         , type=int, required=False, default=10)
-    parser.add_argument("-sc", "--starting_character_set", help="The character set to us for the input, default set is "
-                        "'" + Charsets.standard_charset + "' character set", required=False,
-                        default=Charsets.standard_charset)
+    parser.add_argument("-sc", "--starting_character_set", help="The character set to us for the input, by default a "
+                                                                "best attempt will be made at detecting the character "
+                                                                "based off of the starting value", required=False)
     parser.add_argument("-ec", "--ending_character_set", help="The character set to us for the output, default set is "
                         "'" + Charsets.standard_charset + "' character set", required=False,
                         default=Charsets.standard_charset)
     parser.add_argument("starting_value", help="The value to convert from", type=str)
     parser.add_argument("ending_base", help="The base in which the value is to be converted to", type=int)
     args = parser.parse_args()
+
+    if args.starting_character_set is None:
+        args.starting_character_set = Charsets.detect_charset(args.starting_value)
 
     return Base_Converter.BaseConverter(args.starting_base, args.starting_value, args.ending_base,
                                         args.starting_character_set, args.ending_character_set)
