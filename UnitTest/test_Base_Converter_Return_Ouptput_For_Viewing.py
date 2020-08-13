31d917bda51ac0973f2a35b61efbf518aded6796
Poxes <prox.darknet@gmail.com>
2020-08-13 15:35:31 -0400

    Unit test refactoring (#6)
    Better unit test, left older ones in that test a bunch of different values despite not being actual "unit" test as they could still be useful. If one fails in the future the part that fails should probably be refactored down into it's corresponding actual unit test.

---
+++ UnitTest/test_Base_Converter_Return_Ouptput_For_Viewing.py
@@ -0,0 +1,29 @@
+import unittest
+import Base_Converter
+import Charsets
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.converter = Base_Converter.BaseConverter(0, 0, 0)
+
+    def tearDown(self):
+        self.converter = None
+
+    def test_output_value65_standard(self):
+        self.converter.ending_base_value = ["6", "5"]
+        self.assertEqual("65", self.converter.return_output_for_viewing())
+
+    def test_output_value5_standard(self):
+        self.converter.ending_base_value = ["5"]
+        self.assertEqual("5", self.converter.return_output_for_viewing())
+
+    def test_output_valueU0000U0065_unicode(self):
+        self.converter.ending_base_value = ["U", "+", "0", "0", "0", "0", "U", "+", "0", "0", "6", "5"]
+        self.converter.ending_base_charset = Charsets.unicode_charset
+        self.assertEqual("U+0000 U+0065", self.converter.return_output_for_viewing())
+
+
+if __name__ == '__main__':
+    unittest.main()
